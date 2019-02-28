8957b5a733b37903b508de34fbbae1ee331b1f82
ThibCharpy <thibault.charpignon@wanadoo.fr>
2019-02-28 20:52:09 +0100

    Cors filtering added

--- MyCustomBusiness-Application/src/main/java/com/dev/mcb/MyCustomBusinessApplication.java
+++ MyCustomBusiness-Application/src/main/java/com/dev/mcb/MyCustomBusinessApplication.java
@@ -2,24 +2,19 @@ package com.dev.mcb;
 
 import com.dev.mcb.dao.UserDAO;
 import com.dev.mcb.dao.impl.UserDAOImpl;
-import com.dev.mcb.health.DatabaseHealthCheck;
+import com.dev.mcb.filter.CorsServletFilter;
 import com.dev.mcb.mapper.UserMapper;
-import com.dev.mcb.resource.LoginResource;
 import com.dev.mcb.resource.UserResource;
 import io.dropwizard.Application;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.hibernate.ScanningHibernateBundle;
-import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
-import org.jdbi.v3.core.Jdbi;
 import zone.dragon.dropwizard.HK2Bundle;
 
 import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
 import java.util.EnumSet;
 
 public class MyCustomBusinessApplication extends Application<MyCustomBusinessConfiguration> {
@@ -53,7 +48,7 @@ public class MyCustomBusinessApplication extends Application<MyCustomBusinessCon
 
     @Override
     public void run(MyCustomBusinessConfiguration configuration,
-                    Environment environment) throws Exception {
+                    Environment environment) {
 
         // DAOs
         final UserDAO userDAO = new UserDAOImpl(hibernate.getSessionFactory());
@@ -69,25 +64,7 @@ public class MyCustomBusinessApplication extends Application<MyCustomBusinessCon
         // Resources
         environment.jersey().register(new UserResource(userDAO));
 
-        // Other
-        configureCors(environment);
-
-        //environment.healthChecks().register("health",
-                //new DatabaseHealthCheck(jdbi, configuration.getDataSourceFactory().getValidationQuery()));
-    }
-
-    private void configureCors(Environment environment) {
-        final FilterRegistration.Dynamic cors =
-                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
-
-        // Configure CORS parameters
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
-        cors.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "false");
-
-        // Add URL mapping
-        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
-
+        environment.servlets().addFilter("CorsServletFilter", new CorsServletFilter())
+                .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
     }
 }
---
+++ MyCustomBusiness-Application/src/main/java/com/dev/mcb/filter/CorsServletFilter.java
@@ -0,0 +1,39 @@
+package com.dev.mcb.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class CorsServletFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = ((HttpServletResponse) servletResponse);
+        httpServletResponse.addHeader(
+                "Access-Control-Allow-Origin", "*");
+        httpServletResponse.addHeader(
+                "Access-Control-Allow-Headers",
+                "origin, content-type, accept");
+        httpServletResponse.addHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+
+        filterChain.doFilter(servletRequest, httpServletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
